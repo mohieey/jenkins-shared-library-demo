041efef1e63e31a82a0716ae1244f51df3e856a4
mohieey <mohhiey@gmail.com>
2022-05-25 23:30:56 +0200

    add new func

--- vars/sayName.groovy
+++ vars/sayName.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
 def call(String name) {
     echo "====================The best player is ${name}===================="
-    echo "The Branch Name is $BRANCH_NAME"
+    echo "The Branch Name is ${BRANCH_NAME}"
 }
